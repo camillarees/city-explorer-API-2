d1627894c99957f38d54e67efe57409857a48e51
Camilla Rees <camilla.c.rees@gmail.com>
2022-10-19 23:11:29 -0600

    lab 10 caching

--- modules/movies.js
+++ modules/movies.js
@@ -1,20 +1,30 @@
 'use strict';
 
 const axios = require('axios');
+let cache = require('./cache.js');
 
 const getMovieData = async (request, response, next) => {
-    const city = request.query.city;
-    console.log(city);
     try {
-        const moviesURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&query=${city}`
+        const city = request.query.city;
+        const movieKey = process.env.REACT_APP_MOVIE_API_KEY;
+        const moviesURL = `https://api.themoviedb.org/3/search/movie?api_key=${movieKey}&query=${city}`;
         const movieResponse = await axios.get(moviesURL);
-        const movieArray = movieResponse.data.results.map(movie => new Movie(movie));
-        console.log(movieResponse.data);
-        response.status(200).send(movieArray);
+        const key = 'movie-' + request.query.searchQuery;
+
+        if (cache[key] && (Date.now() - cache[key].timestamp < 100000)) {
+            console.log('movie cache hit');
+        } else {
+            console.log('movie cache miss');
+            cache[key] = {};
+            cache[key].timestamp = Date.now();
+            cache[key].data = movieResponse.data.results.map(movie => new Movie(movie));
+        }
+        response.send(cache[key].data)
     } catch (error) {
         next(error.message);
     }
 }
+
 
 class Movie {
     constructor(movie) {
@@ -27,5 +37,8 @@ class Movie {
         this.releaseDate = movie.release_date;
     }
 }
+
+
+
 
 module.exports = getMovieData;
--- modules/weather.js
+++ modules/weather.js
@@ -2,24 +2,36 @@
 
 const axios = require('axios');
 
+const cache = require('./cache.js');
+
 // define an endpoint that gets the weather data and returns it to React
-const getWeatherData = async (request, response, next) => {
-    const lat = request.query.lat;
-    const lon = request.query.lon;
-    // console.log(lat, lon);
-    try {
         //grab the searchQuery from the request object 
         // baseURL, endpoint, query, queryParameters
-        const weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.REACT_APP_WEATHER_KEY}&lat=${lat}&lon=${lon}&days=5`
-        const weatherResponse = await axios.get(weatherURL);
-        console.log(weatherResponse.data.data[0]);
-        const weatherArray = weatherResponse.data.data.map(forecast => new Forecast(forecast));
 
-        response.status(200).send(weatherArray);
+const getWeatherData = async (request, response, next) => {
+    try {
+        const lat = request.query.lat;
+        const lon = request.query.lon;
+        const weatherKey = process.env.REACT_APP_WEATHER_KEY;
+        const weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?key=${weatherKey}&lat=${lat}&lon=${lon}&days=5`
+        const weatherResponse = await axios.get(weatherURL);
+        const key = 'weather-' + lat + lon;
+ 
+        if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+            console.log('weather cache hit');
+        } else {
+            console.log('weather cache miss');
+            cache[key] = {};
+            cache[key].timestamp = Date.now();
+            cache[key].data = weatherResponse.data.data.map(forecast => new Forecast(forecast, cache[key].timestamp));
+        }
+        response.send(cache[key].data);
+
     } catch (error) {
         next(error.message);
     }
-};
+}
+
 
 class Forecast {
     constructor(forecast) {
@@ -30,6 +42,6 @@ class Forecast {
 
 }
 
-
-
 module.exports = getWeatherData;
+
+
